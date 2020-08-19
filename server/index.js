3e8774eb4b8b6e5f28445b1b8eadaee806e724f5
mhpicel <mhpicel@gmail.com>
2020-08-19 01:56:18 -0500

    Modules now rendering. Data queries not working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 var express = require('express')
+var { createProxyMiddleware } = require('http-proxy-middleware');
+var cors = require('cors');
 var app = express();
+app.use(cors());
+app.use(express.static(__dirname + '/../public'));
 
-var port = 3000;
+console.log('cpm', createProxyMiddleware)
+
+var port = 3004;
+
+app.use('/:id/pictures/:id', createProxyMiddleware({
+  target: 'http://localhost:3000/',
+  changeOrigin: true,
+}));
+app.use('/:id/similar/:id', createProxyMiddleware({
+  target: 'http://localhost:3001/',
+  changeOrigin: true,
+}));
+app.use('/reviews', createProxyMiddleware({
+  target: 'http://localhost:3002/',
+  changeOrigin: true,
+}));
+app.use('/:id/bag/:id', createProxyMiddleware({
+  target: 'http://localhost:3003/',
+  changeOrigin: true,
+}));
 
 app.get('/', (req, res) => {
-
+  res.send('Mykea homepage');
 });
 
 app.listen(port, () => {
